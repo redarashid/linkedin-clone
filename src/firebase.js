d31ac2ab5729a7b915070c8751b8149490021d84
redarashid <redarashid824@gmail.com>
2024-11-28 12:52:11 +0200

    linkedin-clone

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY ,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID ,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID ,
-  };
+  apiKey: import.meta.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.VITE_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const provide = new GoogleAuthProvider()
+
+export { auth, db, provide };
+
+
+
