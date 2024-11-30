ae8d60eafcb2951b9fd45e4eac6403c0ae2daf8c
redarashid <redarashid824@gmail.com>
2024-11-30 13:23:12 +0200

    linkedin-clone

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,49 @@
+import { useState } from "react";
+import { connect } from "react-redux";
+import styled from "styled-components";
 
-const Main = () => {
+const Main = (props) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(!showModal);
+  };
   return (
-    <div>Main</div>
-  )
-}
+    <div>
+      <Container>
+        <ShareBox>
+          <div>
+            {props.user && props.user.photoURL ? (
+              <img src={props.user.photoURL} alt="" />
+            ) : (
+              <img src="/images/user.svg" alt="" />
+            )}
+            <button
+              onClick={handleClick}
+              disabled={props.loading ? true : false}>
+              Start a post
+            </button>
+          </div>
+        </ShareBox>
+      </Container>
+    </div>
+  );
+};
 
-export default Main
+const Container = styled.div``;
+const ShareBox = styled(CommonCard)``;
+
+const mapStateToProps = (state) => {
+  return {
+    loading: state.articleState.loading,
+    user: state.userState.user,
+    articles: state.articleState.articles,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getArticles: () => dispatch(getArticlesAPI()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
