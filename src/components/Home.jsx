0b0c14398385853e1197b99d605cb316c22806bd
redarashid <redarashid824@gmail.com>
2024-11-30 12:38:48 +0200

    linkedin-clone

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,33 @@
+import styled from "styled-components";
+import Leftside from "./Leftside";
+import Rightside from "./Rightside";
+import Main from "./Main";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Container>
+        <Section>
+          <h5>
+            <a href="#">Hiring in a hurry? - </a>
+          </h5>
+          <p>
+            Find talented pros in record time with Upwork and keep business
+            moving.
+          </p>
+        </Section>
+        <Layout>
+          <Leftside />
+          <Main />
+          <Rightside />
+        </Layout>
+      </Container>
+    </div>
+  );
+};
 
-export default Home
+const Container = styled.div``;
+const Section = styled.section``;
+const Layout = styled.div``;
+
+export default Home;
